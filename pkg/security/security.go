6cce3b6e20e5205bf650cabb18aa0cf5604223c9
Caleb Matsick <calebmatsick@gmail.com>
2021-04-15 07:46:45 -0700

    Migrate encryption methods to new pkg

---
+++ pkg/security/security.go
@@ -0,0 +1,68 @@
+package security
+
+import (
+	// Standard
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+
+func encrypt(input string) []byte {
+	mes := []byte(input)
+	key := []byte("passphrasewhichneedstobe32bytes!")
+
+	c, err := aes.NewCipher(key)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		fmt.Println(err)
+	}
+
+	mes = gcm.Seal(nonce, nonce, mes, nil)
+
+	return mes
+}
+
+
+func decrypt(ciphertext []byte) string {
+	key := []byte("passphrasewhichneedstobe32bytes!")
+
+	c, err := aes.NewCipher(key)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nonceSize := gcm.NonceSize()
+
+	if len(ciphertext) < nonceSize {
+		fmt.Println(err)
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	decCmd, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(decCmd)
+}
