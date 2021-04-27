d3d630b1a555d4c455184c25a5e53be35884c701
Caleb Matsick <calebmatsick@gmail.com>
2021-04-27 14:02:20 -0700

    Initial commit

---
+++ pkg/transfer/transfer.go
@@ -0,0 +1,81 @@
+package transfer
+
+import (
+	// Standard
+	"encoding/gob"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+
+	// C3
+	"github.com/calebmatsick/C3/pkg/security"
+)
+
+
+const BUFFERSIZE = 1024
+
+
+func fillString(returnString string, toLength int) string {
+	for {
+		lengthString := len(returnString)
+		if lengthString < toLength {
+				returnString = returnString + ":"
+				continue
+		}
+		break
+	}
+	return returnString
+}
+
+
+func sendFile(conn net.Conn, givenFile string) {
+	fmt.Println("")
+	enc := gob.NewEncoder(conn)
+	file, err := os.Open(givenFile)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+	fileName := fillString(fileInfo.Name(), 64)
+	fmt.Println("Sending filename and filesize!")
+	encFileSize := security.Encrypt(fileSize)
+	enc.Encode(encFileSize)
+	encFileName := security.Encrypt(fileName)
+	enc.Encode(encFileName)
+	sendBuffer := make([]byte, BUFFERSIZE)
+	fmt.Println("Start sending file!")
+	for {
+		_, err = file.Read(sendBuffer)
+		if err == io.EOF {
+			break
+		}
+		encSendBuffer := security.Encrypt(string(sendBuffer))
+		enc.Encode(encSendBuffer)
+	}
+	fmt.Println("File has been sent")
+}
+
+
+func recieveFile(conn net.Conn) {
+	dec := gob.NewDecoder(conn)
+	fileName :=
+	fileSize :=
+
+	for {
+		if (fileSize - recievedBytes) < BUFFERSIZE {
+			io.CopyN(newFile, conn, (fileSize - recievedBytes))
+			dec.Decode(make([]byte, (reciveedBytes+BUFFERSIZE)))
+			break
+		}
+		
+	}
+}
